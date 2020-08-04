46ffb00979146fdef226d42e59256d1a59869832
Igor Aleksanov <popzxc@yandex.ru>
2020-08-04 11:21:10 +0300

    Add crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! Messages. Convenient asynchronous communication
+//! 
+//! `messages` is a very simplistic library, which provides a more declarative interface than raw channels, but yet
+//! not overcomplicates things with too much functionality.
+//! 
+//! It is intended to be used when channels in your project start looking a bit messy, but you aren't sure that
+//! migrating to the actor framework is a right choice.
+
 pub use crate::{
     address::Address,
     errors::{ReceiveError, SendError},
