f4553c04d53512dfa235dd49fd841b29c6e06161
Igor Aleksanov <popzxc@yandex.ru>
2021-06-03 13:28:28 +0300

    Add raw channels benchmark for comparison

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,8 @@
 use criterion::*;
+use futures::{
+    channel::{mpsc, oneshot},
+    SinkExt, StreamExt,
+};
 use messages::prelude::*;
 
 struct Ping;
@@ -30,6 +34,8 @@ impl Notifiable<u8> for Ping {
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    // Actors benchmarks.
+
     let mut g = c.benchmark_group("Actor workflow");
     g.throughput(Throughput::Elements(1));
 
@@ -53,6 +59,32 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             || Ping.spawn(),
             |mut addr| async move {
                 addr.notify(20u8).await.unwrap();
+            },
+        )
+    });
+
+    g.finish();
+
+    // Raw channel benchmarks.
+
+    let mut g = c.benchmark_group("Raw channel throughput");
+    g.throughput(Throughput::Elements(1));
+
+    g.bench_function("Actor send message", move |b| {
+        b.to_async(runtime()).iter_with_setup(
+            || {
+                let (oneshot_send, oneshot_recv) = oneshot::channel::<u8>();
+                let (send, mut recv) = mpsc::channel::<oneshot::Sender<u8>>(16);
+                tokio::spawn(async move {
+                    let send = recv.next().await.unwrap();
+                    send.send(42u8).unwrap();
+                });
+
+                (send, oneshot_send, oneshot_recv)
+            },
+            |(mut send, back_send, recv)| async move {
+                let _x = send.send(back_send).await.unwrap();
+                let _y = black_box(recv.await.unwrap());
             },
         )
     });
