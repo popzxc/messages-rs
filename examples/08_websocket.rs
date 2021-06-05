8bb7ff7c6ef38624f1e7fc4c74bce02e18c60b3b
Igor Aleksanov <popzxc@yandex.ru>
2021-06-05 08:44:01 +0300

    Complete websocket example

--- examples/08_websocket.rs
+++ examples/08_websocket.rs
@@ -70,8 +70,57 @@ mod server {
 }
 
 mod client {
-    pub(super) async fn run(_addr: String) {
-        todo!()
+    //! Client implementation is respectfull borrowed from [`tokio-tungstenite`] [example].
+    //! It does not use actors, and is put here just so you can play with the server.
+    //!
+    //! [`tokio-tungstenite`]: https://github.com/snapview/tokio-tungstenite
+    //! [example]: https://github.com/snapview/tokio-tungstenite/blob/master/examples/client.rs
+
+    use futures::{future, pin_mut, StreamExt};
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+    pub(super) async fn run(connect_addr: String) {
+        if !connect_addr.starts_with("ws://") {
+            eprintln!("Server URL must start with `ws://`");
+            return;
+        }
+
+        let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
+        tokio::spawn(read_stdin(stdin_tx));
+
+        let (ws_stream, _) = connect_async(connect_addr)
+            .await
+            .expect("Failed to connect");
+        println!("WebSocket handshake has been successfully completed");
+
+        let (write, read) = ws_stream.split();
+
+        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+        let ws_to_stdout = {
+            read.for_each(|message| async {
+                let data = message.unwrap().into_data();
+                tokio::io::stdout().write_all(&data).await.unwrap();
+            })
+        };
+
+        pin_mut!(stdin_to_ws, ws_to_stdout);
+        future::select(stdin_to_ws, ws_to_stdout).await;
+    }
+
+    // Our helper method which will read data from stdin and send it along the
+    // sender provided.
+    async fn read_stdin(tx: futures::channel::mpsc::UnboundedSender<Message>) {
+        let mut stdin = tokio::io::stdin();
+        loop {
+            let mut buf = vec![0; 1024];
+            let n = match stdin.read(&mut buf).await {
+                Err(_) | Ok(0) => break,
+                Ok(n) => n,
+            };
+            buf.truncate(n);
+            tx.unbounded_send(Message::binary(buf)).unwrap();
+        }
     }
 }
 
