a84f74b633c278e3d136dfc278a7880fecd66fb0
Igor Aleksanov <popzxc@yandex.ru>
2021-06-06 10:57:00 +0300

    Remove thiserror dependency

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,16 @@
 //! Errors that can occur during the actor interaction workflow.
 
-use thiserror::Error;
-
 /// Errors that can occur while sending the message.
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum SendError {
     /// Error emitted when it was attempted to send a message to the stopped actor.
-    #[error("Actor does not accepting messages")]
     ReceiverDisconnected,
 }
+
+impl std::fmt::Display for SendError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Actor does not accepting messages")
+    }
+}
+
+impl std::error::Error for SendError {}
